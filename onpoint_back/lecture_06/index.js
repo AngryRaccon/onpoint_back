0906d8da4a1830959a8f44ea2da72308dcf5ec7f
AngryRaccon <kmagalinskaya@yandex.ru>
2021-08-27 20:52:05 +0300

    api router

--- onpoint_back/lecture_06/index.js
+++ onpoint_back/lecture_06/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000; //TODO запуск на любом порту
+const api = express();
 const booksRouter = require("./routes/books");
 const userRouter = require("./routes/user");
 
 app.use(express.json());
-app.use("/api/books", booksRouter);
-app.use("/api/user", userRouter);
+api.use("/books", booksRouter);
+api.use("/user", userRouter);
+
+app.use("/api", api);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
